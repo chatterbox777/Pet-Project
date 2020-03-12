eab1733b3a382939f6ca2918d178e4e0e73f0a9d
Vasya <chatterbox1995@mail.ru>
2020-03-12 09:21:37 +0300

    2
    made counter

--- src/App.js
+++ src/App.js
@@ -18,8 +18,17 @@ class App extends React.Component {
       { id: 2, songName: "Светофоры", artist: "Леша Свик", isOn: false },
       { id: 3, songName: "Da Da Da", artist: "Tanir, Tyomcha", isOn: false },
       { id: 4, songName: "Чужая", artist: "Артур Пирожков", isOn: false }
-    ]
+    ],
+    count: 0
   };
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
   render() {
     return (
       <BrowserRouter>
@@ -30,8 +39,25 @@ class App extends React.Component {
             path="/Music"
             render={() => <Music items={this.state.items} />}
           />
+          <Form
+            counter={this.state.count}
+            increment={this.increment}
+            decrement={this.decrement}
+          />
         </div>
       </BrowserRouter>
+    );
+  }
+}
+
+class Form extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="form">{this.props.counter}</div>
+        <button onClick={this.props.increment}>+</button>
+        <button onClick={this.props.decrement}>-</button>
+      </div>
     );
   }
 }
--- src/Components/Music/PlayButton.jsx
+++ src/Components/Music/PlayButton.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import classTags from "../Music/PlayButton.module.css";
-const PlayButton = () => {
-  return <button className={classTags.btn}>Play</button>;
-};
+import AudioPlayer from "react-modular-audio-player";
+import Uhozu from "./Uhozu.mp3";
+
+let playlist = [
+  { src: { Uhozu }, title: "Song", artist: "Singer" },
+  { src: { Uhozu }, title: "Another Song", artist: "Another Singer" }
+];
+
+class PlayButton extends React.Component {
+  state = {};
+  render() {
+    return <AudioPlayer audioFiles={playlist} />;
+  }
+}
 
 export default PlayButton;
