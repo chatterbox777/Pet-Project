04c824925c49877e611ac30b17b2d5579c68b1e7
Vasya <chatterbox1995@mail.ru>
2020-05-18 20:36:51 +0300

    30
    make some update

--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -7,6 +7,7 @@ export class Weather extends React.Component {
     temperature: "",
     place: "",
     pressure: "",
+    sunset: "",
   };
 
   // получаем значение, введенное юзером в input
@@ -26,10 +27,20 @@ export class Weather extends React.Component {
         `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=4a2600a79372ba51d19f02956a80ce0b&units=metric`
       )
       .then((response) => {
+        debugger;
+        var sunset = response.data.sys.sunset;
+        console.log(sunset);
+        var date = new Date();
+        console.log(date);
+        // неправильно работает , надо исправить чтобы верно отображало заход солнца
+        date.setTime(sunset);
+        console.log(date);
+        var sunset_date = `${date.getHours()} :  ${date.getMinutes()}: ${date.getSeconds()}`;
         this.setState({
           place: response.data.name,
           temperature: response.data.main.temp + "°С",
           pressure: response.data.main.pressure + "мм рт. ст.",
+          sunset: sunset_date,
         });
         console.log(response);
       });
@@ -49,7 +60,7 @@ export class Weather extends React.Component {
         <p>Местоположение: {this.state.place}</p>
         <p>Температура: {this.state.temperature} </p>
         <p>Давление: {this.state.pressure} </p>
-        <p>Заход солнца: </p>
+        <p>Заход солнца: {this.state.sunset}</p>
       </div>
     );
   }
